73887e2b199aa3a8a478e9a68f0c6b9e3f204ef5
Jehoi-ga-ada <sayajehoiada@gmail.com>
2025-07-06 16:07:14 +0700

    changed file name to page, enabling rendering

---
+++ src/app/dashboard/rpp/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { DownloadIcon, PlusIcon } from "lucide-react"
+import { RPPTable } from "@/components/rpp/RPPTable"
+import { useState } from "react"
+
+export default function RPPPage() {
+  const [searchQuery, setSearchQuery] = useState("")
+  return (
+    <div className="p-6 space-y-6">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">List RPP</h1>
+        <div className="flex justify-end space-x-4">
+          <Input placeholder="Search..." 
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="w-1/3" />
+          <Button>
+            <PlusIcon className="mr-2 h-4 w-4" />
+            Add New
+          </Button>
+        </div>  
+      </div>
+
+      <RPPTable searchQuery={searchQuery}/>
+    </div>
+  )
+}
