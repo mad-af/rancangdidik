8f01a8b974fcdf88d8f11eb5f5da5b541a1c69a3
Jehoi-ga-ada <sayajehoiada@gmail.com>
2025-07-07 21:29:59 +0700

    changed to original size

--- src/assets/icons/Calendar.tsx
+++ src/assets/icons/Calendar.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 const Calendar = (props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 10 10"
+    viewBox="0 0 20 20"
     fill="currentColor"
     {...props}
   >
